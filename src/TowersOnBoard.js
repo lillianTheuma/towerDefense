7fe8d59e73caefa64571f94177addbe955abf3c8
Lillian Theuma <53153850+lillianTheuma@users.noreply.github.com>
2019-12-02 11:49:08 -0800

    Tower Object Code (#3)
    * flushed out javascript code
    
    * progressed tower objects.

---
+++ src/TowersOnBoard.js
@@ -0,0 +1,19 @@
+export default class towersOnBoard {
+  constructor() {
+    this.towers = [false, false, false, false, false],
+    this.currentId = 0;
+  }
+  findTowerByID(id) {
+    this.towers.forEach(function(tower) {
+      if tower.id = id {
+        return tower;
+      }
+    });
+  return false;
+  }
+  addTower(tower, position) {
+    tower.id = this.currentId;
+    this.currentId++;
+    this.towers[position] = tower;
+  }
+}
---
+++ src/towerTypes.js
@@ -0,0 +1,12 @@
+export default class towerTypes {
+  constructor() {
+    this.types = [];
+  }
+  findTowerByID(id) {
+    this.towers.forEach(function(tower) {
+      if tower.id = id {
+        return tower;
+      }
+    });
+  retu
+}
