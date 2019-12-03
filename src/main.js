d70df5b6a02d15733aef76bc1e9273ef52e8841f
Lillian Theuma <53153850+lillianTheuma@users.noreply.github.com>
2019-12-02 16:00:34 -0800

    Lee display logic (#9)
    * add to main.js
    
    * add to main.js
    
    * troubleshooting

--- src/main.js
+++ src/main.js
@@ -12,20 +12,44 @@ import GameState from './gameState.js';
 import towers from './towers.json';
 
 $(document).ready(function() {
-  $('').submit(function(event) {
+  $('#start').submit(function(event) {
     event.preventDefault();
-     var game = new gameState;
-     var gameStudents = new StudentsOnBoard;
-     var gameTowers = new towersOnBoard;
+     let game = new GameState;
+     let creationMode = null;
+     let time = 0;
 
     setInterval(() => {
       if (game.health > 0) {
-        gameStudents.advanceStudents();
-        for (let i = 0; i < 20; i++) {
-          // if (space id matches
+        time++;
+        console.log(time);
+
+        // Adding students every second
+        if (time < 10) {
+          game.students.addStudent(new StudentType[0]);
+        } else if (time < 20) {
+          game.students.addStudent(new StudentType[1]);
+        } else if (time < 30) {
+          game.students.addStudent(new StudentType[2]);
+        } else {
+          game.students.addStudent(new StudentType[3]);
+        }
+        game.students.advanceStudents();
+
+        // Pruning out dead students
+        for (let i = 0; i < game.students.length; i++) {
+          if (game.students[i].health < 1) {
+            game.students.removeStudent(i);
+          }
+          if (game.students[i].progress > 10) {
+            game.health -= 10;
+          }
         }
 
+        game.money += 10;
+      } else {
+        // you lost function
       }
+
       // board.checkIfWon();
       // board.time++;
       // students.addStudent(board.time);
@@ -33,16 +57,21 @@ $(document).ready(function() {
 
     }, 1000);
 
-    // addStudent(time) {
-    //   if (time < 10 seconds) {
-    //     this.students.push(new StudentType.types[0]);
-    //   } else if (time < 20 seconds) {
-    //     this.students.push(new StudentType.types[1]);
-    //   } else if (time < 30 seconds) {
-    //     this.students.push(new StudentType.types[2]);
-    //   } else if (time >= 30 seconds) {
-    //     this.students.push(new StudentType.types[3]);
-    //   }
-    // }
+    $("#makeTower1").click(function() {
+      creationMode = towerType1;
+    });
+
+    // may not work correctly because of weird timing things
+    function attachContactListeners() {
+      for (let i = 0; i < 5; i++) {
+        $(".spaces").on("click", "p#" + i, function() {
+          // if (spaceHasTower) {
+          //   game.upgradeTower();
+          // } else {
+          //   game.buyTower(creationMode, i);
+          // }
+        });
+      }
+    }
   });
 });
