ff4b848a6c91d5b383e46bf6eb892a38c25271a5
Student <example@example.com>
2019-12-04 14:49:29 -0800

    clean up main.js to remove excess sprite tests. Adds some commented instructions for new sprites

--- src/main.js
+++ src/main.js
@@ -37,90 +37,17 @@ import tower2_t3R from './img/gameAssets/tower2_tier3R.png';
 import tower3_t3L from './img/gameAssets/tower3_tier3L.png';
 import tower3_t3R from './img/gameAssets/tower3_tier3R.png';
 
-// Make IMG Tags----------------------
-const sprite_tower1_t1 = new Image();
-const sprite_tower1_t2 = new Image();
-const sprite_tower1_t3 = new Image();
+// How to make a new Sprite:
+  // Make IMG Tags----------------------
+  // const sprite_tower1_t1 = new Image();
 
-const sprite_tower2_t1 = new Image();
-const sprite_tower2_t2 = new Image();
-const sprite_tower2_t3 = new Image();
+  // Set URLs --------------------------
+  // sprite_tower1_t1.src = tower1_t1;
 
-const sprite_tower3_t1 = new Image();
-const sprite_tower3_t2 = new Image();
-const sprite_tower3_t3 = new Image();
+// How to display a Sprite to a coordinate in the grid:
+  // $("ID_OF_SPACE").append(sprite_tower1_t1);
 
-const sprite_tower1_t3L = new Image();
-const sprite_tower1_t3R = new Image();
-const sprite_tower2_t3L = new Image();
-const sprite_tower2_t3R = new Image();
-const sprite_tower3_t3L = new Image();
-const sprite_tower3_t3R = new Image();
-
-
-const sprite_zombieUp = new Image();
-const sprite_zombieDown = new Image();
-const sprite_zombieLeft = new Image();
-const sprite_zombieRight = new Image();
-
-// Set URLs --------------------------
-sprite_tower1_t1.src = tower1_t1;
-sprite_tower1_t2.src = tower1_t2;
-sprite_tower1_t3.src = tower1_t3;
-
-sprite_tower2_t1.src = tower2_t1;
-sprite_tower2_t2.src = tower2_t2;
-sprite_tower2_t3.src = tower2_t3;
-
-sprite_tower3_t1.src = tower3_t1;
-sprite_tower3_t2.src = tower3_t2;
-sprite_tower3_t3.src = tower3_t3;
-
-sprite_tower1_t3L.src = tower1_t3L;
-sprite_tower1_t3R.src = tower1_t3R;
-sprite_tower2_t3L.src = tower2_t3L;
-sprite_tower2_t3R.src = tower2_t3R;
-sprite_tower3_t3L.src = tower3_t3L;
-sprite_tower3_t3R.src = tower3_t3R;
-
-sprite_zombieUp.src = zombieUp;
-sprite_zombieDown.src = zombieDown;
-sprite_zombieLeft.src = zombieLeft;
-sprite_zombieRight.src = zombieRight;
 // -------------------------------------------------------
-
-$(".spriteDump").append(sprite_tower1_t1);
-$(".spriteDump").append(sprite_tower1_t2);
-$(".spriteDump").append(sprite_tower1_t3);
-
-$(".spriteDump").append(sprite_tower2_t1);
-$(".spriteDump").append(sprite_tower2_t2);
-$(".spriteDump").append(sprite_tower2_t3);
-
-$(".spriteDump").append(sprite_tower3_t1);
-$(".spriteDump").append(sprite_tower3_t2);
-$(".spriteDump").append(sprite_tower3_t3);
-
-$(".spriteDump").append(sprite_tower1_t3L);
-$(".spriteDump").append(sprite_tower1_t3R);
-
-$(".spriteDump").append(sprite_tower2_t3L);
-$(".spriteDump").append(sprite_tower2_t3R);
-
-$(".spriteDump").append(sprite_tower3_t3L);
-$(".spriteDump").append(sprite_tower3_t3R);
-
-$(".spriteDump").append(sprite_zombieUp);
-$(".spriteDump").append(sprite_zombieDown);
-$(".spriteDump").append(sprite_zombieRight);
-$(".spriteDump").append(sprite_zombieLeft);
-
-// Testing putting a sprite on the grid
-
-$("#0x1").html(sprite_tower3_t3L);
-$("#0x2").html(sprite_tower3_t3R);
-$("#0x3").html(sprite_tower3_t3L);
-$("#0x4").html(sprite_tower3_t3R);
 
 
 $(document).ready(function() {
