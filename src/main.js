30d65d50c939b5d67887c3986f995820d0a7d271
lioness3 <jcnailtech3@yahoo.com>
2019-12-04 08:14:03 -0800

    remove button style

--- src/main.js
+++ src/main.js
@@ -12,72 +12,74 @@ import GameOver from './img/gameOver.jpg';
 //////////////////////
 //ignore this please for jo  :)
 // $(document).ready(function() {
-//   $('.apocalypse').addClass();
 //   $('.apocalypse').removeClass();
 //  $('.jail').addClass();
-// $('.jail').removeClass();
 // $("#brainImage").
 // attr('src','./img/brain.png');
 //
 //
 // attr("<img src='"+Brain+"' alt='Image of a brain'>");
+
+// if (gameover){
+//     $('.apocalypse').addClass();
+// $('.jail').removeClass();
+//
+// }
 ///////////////////////////////
 
-  $('#start').click(function(event) {
-     $('#start').hide();
-     let game = new GameState();
-     let creationMode = null;
-     let time = 0;
-     let newStudent = new StudentType;
-     let spaceNumber = 53;
-     let towerNumber = 5;
-     attachContactListeners();
-
-    setInterval(() => {
-      if (game.health > 0) {
-        console.log(time);
-        time++;
-
-        // Adding students every second
-        if (time < 10) {
-          game.students.addStudent(newStudent.types[0]);
-        } else if (time < 20) {
-          game.students.addStudent(newStudent.types[1]);
-        } else if (time < 30) {
-          game.students.addStudent(newStudent.types[2]);
-        } else {
-          game.students.addStudent(newStudent.types[3]);
-        }
-        game.students.advanceStudents();
-        game.students.checkHealth();
-
-        // Increment game health down - gameState.js
-
-        // for (let i = 0; i < game.students.length; i++) {
-        //   if (game.students[i].progress > 10) {
-        //     game.health -= 10;
-        //   }
-        // }
-
-        game.money += 10;
-      } else {
-        console.log("you lost");
-      }
-
-      // board.checkIfWon();
-      // students.addStudent(board.time);
-
-    }, 1000);
-
-    function attachContactListeners() {
-      $("#towerSelection").on("click", "button", function() {
-        if (this.id == "tower3") {
-          creationMode = game.towers.types[0];
-          console.log("tower3");
-        }
-        console.log("should print out name of button: " + this.id);
-      });
-    }
-  });
-
- });
+  // $('#start').click(function(event) {
+  //    $('#start').hide();
+  //    let game = new GameState();
+  //    let creationMode = null;
+  //    let time = 0;
+  //    let newStudent = new StudentType;
+  //    let spaceNumber = 53;
+  //    let towerNumber = 5;
+  //    attachContactListeners();
+  //
+  //   setInterval(() => {
+  //     if (game.health > 0) {
+  //       console.log(time);
+  //       time++;
+  //
+  //       // Adding students every second
+  //       if (time < 10) {
+  //         game.students.addStudent(newStudent.types[0]);
+  //       } else if (time < 20) {
+  //         game.students.addStudent(newStudent.types[1]);
+  //       } else if (time < 30) {
+  //         game.students.addStudent(newStudent.types[2]);
+  //       } else {
+  //         game.students.addStudent(newStudent.types[3]);
+  //       }
+  //       game.students.advanceStudents();
+  //       game.students.checkHealth();
+  //
+  //       // Increment game health down - gameState.js
+  //
+  //       // for (let i = 0; i < game.students.length; i++) {
+  //       //   if (game.students[i].progress > 10) {
+  //       //     game.health -= 10;
+  //       //   }
+  //       // }
+  //
+  //       game.money += 10;
+  //     } else {
+  //       console.log("you lost");
+  //     }
+  //
+  //     // board.checkIfWon();
+  //     // students.addStudent(board.time);
+  //
+  //   }, 1000);
+  //
+  //   function attachContactListeners() {
+  //     $("#towerSelection").on("click", "button", function() {
+  //       if (this.id == "tower3") {
+  //         creationMode = game.towers.types[0];
+  //         console.log("tower3");
+  //       }
+  //       console.log("should print out name of button: " + this.id);
+  //     });
+  //   }
+  // });
